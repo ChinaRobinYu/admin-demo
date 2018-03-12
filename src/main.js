5b37e76f6af2f2fc842c87355ba175a021eb15db
ztjy-fe <zhangzhenglong@szy.cn>
2018-03-12 20:48:21 +0800

    update

--- src/main.js
+++ src/main.js
@@ -9,15 +9,14 @@ import VueLazyload from 'vue-lazyload'
 import '@/directives/statistic.js'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en'
 // 全局样式
 import '@/assets/scss/index.scss'
 // 路由鉴权
 import '@/utils/permission'
-Vue.use(ElementUI, { locale })
+Vue.use(ElementUI)
 
-const fundebug = require('fundebug-javascript')
 // apikey 可根据实际情况填写
+const fundebug = require('fundebug-javascript')
 fundebug.apikey = '1d6ad910c71b779a7a695fb9b4c71b1d094a3440c0068446ef94ac0aebfb1baa'
 Vue.use(VueRouter)
 Vue.use(Vuex)
